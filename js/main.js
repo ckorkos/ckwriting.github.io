7c3e4eb076a4eeae54a51b0d1f0bc5d7b7e7e545
ckorkos <chkorkos@gmail.com>
2023-03-20 01:25:25 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-// Page Width Breakpoints
-$(document).ready(function() {
-	if($(window).width() >= 1080) {
-
-		console.log("Screen Width: Greater than 1080px");
-	}
-    else if($(window).width() <= 1080) {
-        $(".project-header").width = "90%";
-        $(".project-header").paddingLeft = "5%";
-        $(".project-header").paddingRight = "5%";
-
-		console.log("Screen Width: 1080px");
-	}
-});
-
-$(window).resize(function() {
-    // Copy all breakpoints code here
-
-	if($(window).width() >= 1080) {
-
-		console.log("Screen Width: Greater than 1080px");
-	}
-    else if($(window).width() <= 1080) {
-        $(".project-header").width = "90%";
-        $(".project-header").paddingLeft = "5%";
-        $(".project-header").paddingRight = "5%";
-
-		console.log("Screen Width: 1080px");
-	}
-});
-
 // Change Project Thumbnails on Hover-In
 function changeBG(project) {
     if(project==="skywind-image") {
@@ -42,5 +11,55 @@ function revertBG(project) {
     if(project==="skywind-image") {
         document.getElementById(project).src = "./files/images/thumbnails/skywind-thumb.png";
         document.getElementById("skywind-card-text").style.opacity = 0;
+    }
+}
+
+// Open Hamburger Menu
+function openHamburger() {
+    $(".hamburger-menu").css('display', 'flex');
+    $(".project-tab-hamburger").css('display', 'none');
+    $(".project-tab-close").css('display', 'block');
+}
+
+// Open Hamburger Menu
+function closeHamburger() {
+    $(".hamburger-menu").css('display', 'none');
+    $(".project-tab-hamburger").css('display', 'block');
+    $(".project-tab-close").css('display', 'none');
+}
+
+// Skywind - Link Selections
+function changeSkywindTab(tab) {
+    if(tab==="encounters") {
+        $(".project-tab-name").removeClass("tab-selected");
+        $("#skywind-encounters").addClass("tab-selected");
+
+        // Display Encounters Tab, Hide Others
+        $(".page-content").removeClass("content-selected");
+        $("#content-encounters").addClass("content-selected");
+    }
+    else if(tab==="items") {
+        $(".project-tab-name").removeClass("tab-selected");
+        $("#skywind-items").addClass("tab-selected");
+
+        // Display Items Tab, Hide Others
+        $(".page-content").removeClass("content-selected");
+        $("#content-items").addClass("content-selected");
+    }
+    else if(tab==="quests") {
+        $(".project-tab-name").removeClass("tab-selected");
+        $("#skywind-quests").addClass("tab-selected");
+
+        // Display Quests Tab, Hide Others
+        $(".page-content").removeClass("content-selected");
+        $("#content-quests").addClass("content-selected");
+    }
+    else if(tab==="other") {
+        $(".project-tab-name").removeClass("tab-selected");
+        $("#skywind-other").addClass("tab-selected");
+
+        // Display Other Tab, Hide Others
+        $(".page-content").removeClass("content-selected");
+        $("#content-other").addClass("content-selected");
     }
 }
